91760b390c37542c8f0091c64dc5e5c69c21a65e
qkrqhdtn3 <74765691+qkrqhdtn3@users.noreply.github.com>
2023-12-31 05:43:58 +0900

    add color printing console

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from atexit import register
 from pathlib import Path
 from rich.console import Console
 
-GENERAL = "b bright_white"
+GENERAL = "white"
+PROMPT = "b black"
 
 class ColorfulConsole(Console):
     def print(self, *args, style=GENERAL, highlight=False, **kwargs):
@@ -18,14 +19,30 @@ class Downloader:
 
     def __init__(self):
         self.console = ColorfulConsole()
+        self.running = True
+##        self.register = 
 
     def run(self):
+        self.menu()
+            
+##    def menu(self, selectNum="0"):
+    def menu(self):
         while self.running:
-            prompt(
-                "1",
-                "2",
-                "3"
+            self.prompt(
+                "TikTokDownloader",
+                [
+                "Link",
+                "Login",
+                "Quit"
+                ],
+                self.console
                 )
+            
+    def prompt(self, title: str, contentList: list, console):
+        printConsole = f"{title}\n"
+        for i, j in enumerate(contentList):
+            printConsole += f"{i + 1} {j}\n"
+        return console.input(printConsole)
 
 
 app = Flask(__name__)
