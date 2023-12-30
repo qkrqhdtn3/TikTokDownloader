57550691298320e44bcd892f92dc4cc98d88ded0
qkrqhdtn3 <74765691+qkrqhdtn3@users.noreply.github.com>
2023-12-30 23:58:40 +0900

    fix main.py

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from flask import Flask
 from atexit import register
 from pathlib import Path
+from rich.console import Console
 
+GENERAL = "b bright_white"
+
+class ColorfulConsole(Console):
+    def print(self, *args, style=GENERAL, highlight=False, **kwargs):
+        super().print(*args, style=style, highlight=highlight, **kwargs)
+
+    def input(self, prompt_="", *args, **kwargs):
+        return super().input(
+            f"[{PROMPT}]{prompt_}[/{PROMPT}]", *args, **kwargs)
+    
 class Downloader:
     PROJECT_ROOT = Path('.').resolve().parent
 
-##    def __init__(self):
+    def __init__(self):
+        self.console = ColorfulConsole()
+
+    def run(self):
+        while self.running:
+            prompt(
+                "1",
+                "2",
+                "3"
+                )
 
 
 app = Flask(__name__)
@@ -15,4 +35,5 @@ def home():
     return 'TikTokDownloader'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+##    app.run(debug=True)
+    Downloader().run()
